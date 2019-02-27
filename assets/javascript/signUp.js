c60c21d499d34c0182b73e7788db263fd05a0a42
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-02-27 15:37:47 -0500

    Built signup

---
+++ assets/javascript/signUp.js
@@ -0,0 +1,69 @@
+var config = {
+  apiKey: "AIzaSyAKTJreq0OZgWl8ktgzhd2FvPyYyCYhN1M",
+  authDomain: "blue-lama-retreat-7a0c6.firebaseapp.com",
+  databaseURL: "https://blue-lama-retreat-7a0c6.firebaseio.com",
+  projectId: "blue-lama-retreat-7a0c6",
+  storageBucket: "",
+  messagingSenderId: "106620423709"
+};
+firebase.initializeApp(config);
+
+//call back function
+$("#login").on("click", function() {
+  var email = $("#email").val();
+  var password = $("#password").val();
+
+  const promise = firebase.auth().signInWithEmailAndPassword(email, password);
+  promise.catch(function(event) {
+    console.log(event.message);
+  });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // TODO
+      // Display confirmation
+      // Display name
+      // include Josh code
+
+      // testing. this should direct to the new page
+      var email_id = user.email;
+      $("#paragraph1").text("Welcome " + email_id);
+      $("#paragraph2").text("Your booking date is 2019-03-01");
+
+      console.log(user);
+      logOut.classList.remove("d-none");
+    } else {
+      console.log("not logged in");
+    }
+  });
+});
+
+$("#signUp").on("click", function(event) {
+  var email = $("#email").val();
+  var password = $("#password").val();
+
+  const promise = firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password);
+  promise.catch(function(event) {
+    console.log(event.message);
+  });
+});
+
+$("#password-reset").on("click", function(event) {
+  var email = $("#email").val();
+  firebase
+    .auth()
+    .sendPasswordResetEmail(email)
+    .then(function() {
+      alert("Password Reset Email Sent!");
+    })
+    .catch(function(error) {
+      console.log(error.message);
+    });
+});
+
+$("#logOut").on("click", function(event) {
+  firebase.auth().signOut();
+  $(".container").hide();
+});

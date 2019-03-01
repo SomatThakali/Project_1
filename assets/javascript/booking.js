8c4dd198daa5253871c57e3eec08433b2bdbca58
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-03-01 18:02:52 -0500

    Created confirmation page

--- assets/javascript/booking.js
+++ assets/javascript/booking.js
@@ -26,16 +26,6 @@ var bookingPrice = 0;
 var currentUser;
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    // TODO
-    // make a other functions that wil take
-    // Display confirmation
-    // Display name
-    // include Josh code
-    // window.location = "../templates/booking.html"; // Redirecting to other page
-    // testing. this should direct to the new page
-    // var email_id = user.email;
-    // $("#paragraph1").text("Welcome " + email_id);
-    // $("#paragraph2").text("Your booking date is 2019-03-01");
     currentUser = user;
     console.log(currentUser);
     console.log(user.uid);
@@ -85,7 +75,7 @@ $("#submitButton").on("click", function(event) {
     .trim();
 
   //push to firebase
-  database.ref("/bookings").push({
+  database.ref().push({
     //guest info
     First_name: firstName,
     Last_name: lastName,
@@ -101,33 +91,111 @@ $("#submitButton").on("click", function(event) {
     Total_price: bookingPrice,
     userId: currentUser.uid
   });
+
   window.location = "../templates/confirmation.html";
 });
 
-// if (window.location.href.indexOf("confirmation") > -1) {
-firebase
-  .database()
+// database.ref().on(
+//   "child_added",
+//   function(snapshot) {
+//     $("#firstNameDisplay").text(snapshot.val().First_name);
+//     $("#lastNameDisplay").text(snapshot.val().Last_name);
+//     $("#emailDisplay").text(snapshot.val().Email);
+//     $("#tripDateDisplay").text(snapshot.val().Trip_date);
+//     $("#phoneNumberDisplay").text(snapshot.val().Phone_number);
+//     //dates
+//     $("#bookingDateDisplay").text(snapshot.val().Date_purchased);
+
+//     //location & room
+//     $("#locationBookedDisplay").text(snapshot.val().Location);
+//     $("#roomBookedDisplay").text(snapshot.val().roomBooked);
+//     //price
+//     $("#bookingPriceDisplay").text(snapshot.val().bookingPrice);
+//   },
+//   function(errorObject) {
+//     console.log("Errors handled: " + errorObject.code);
+//   }
+// );
+
+// var email = $("#emailInput").val();
+// database
+//   .ref()
+//   .child("bookings")
+//   .orderByChild("Email")
+//   .equalTo("somatthakali89@gmail.com")
+//   .once("value", function(snapshot) {
+//     console.log(snapshot.val());
+//     snapshot.forEach(function(childSnapshot) {
+//       console.log("in here");
+//       $("#firstNameDisplay").text(childSnapshot.val().First_name);
+//       $("#lastNameDisplay").text(childSnapshot.val().Last_name);
+//       $("#emailDisplay").text(childSnapshot.val().Email);
+//       $("#tripDateDisplay").text(childSnapshot.val().Trip_date);
+//       $("#phoneNumberDisplay").text(childSnapshot.val().Phone_number);
+//       //dates
+//       $("#bookingDateDisplay").text(childSnapshot.val().Date_purchased);
+
+//       //location & room
+//       $("#locationBookedDisplay").text(childSnapshot.val().Location);
+//       $("#roomBookedDisplay").text(childSnapshot.val().roomBooked);
+//       //price
+//       $("#bookingPriceDisplay").text(childSnapshot.val().bookingPrice);
+//     });
+//   });
+// var ref = new Firebase();
+database
   .ref()
-  .child("bookings")
-  .orderByChild("Email")
-  .equalTo("somatthakali89@gmail.com")
-  .once("value", function(snapshot) {
-    console.log(snapshot.val());
-    snapshot.forEach(function(childSnapshot) {
+  .orderByChild("userId")
+  .limitToLast(1)
+  .on(
+    "child_added",
+    function(snapshot) {
+      // renderRow(snapshot);
       console.log("in here");
-      $("#firstNameDisplay").text(childSnapshot.val().First_name);
-      $("#lastNameDisplay").text(childSnapshot.val().Last_name);
-      $("#emailDisplay").text(childSnapshot.val().Email);
-      $("#tripDateDisplay").text(childSnapshot.val().Trip_date);
-      $("#phoneNumberDisplay").text(childSnapshot.val().Phone_number);
+      $("#firstNameDisplay").text(snapshot.val().First_name);
+      $("#lastNameDisplay").text(snapshot.val().Last_name);
+      $("#emailDisplay").text(snapshot.val().Email);
+      $("#tripDateDisplay").text(snapshot.val().Trip_date);
+      $("#phoneNumberDisplay").text(snapshot.val().Phone_number);
       //dates
-      $("#bookingDateDisplay").text(childSnapshot.val().Date_purchased);
+      $("#bookingDateDisplay").text(snapshot.val().Date_purchased);
 
       //location & room
-      $("#locationBookedDisplay").text(childSnapshot.val().Location);
-      $("#roomBookedDisplay").text(childSnapshot.val().roomBooked);
-      //price
-      $("#bookingPriceDisplay").text(childSnapshot.val().bookingPrice);
-    });
-  });
+      $("#locationBookedDisplay").text(snapshot.val().Location);
+      $("#roomBookedDisplay").text(snapshot.val().roomBooked);
+    },
+    function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    }
+  );
+
+// function renderRow(snap) {
+//   var child = snap.val();
+//   var childRef = snap.key;
+//   // console.log("child ", child, childRef);
+
+//   var tRow = $("<tr>");
+//   // tRow.attr("data-ref", childRef);
+
+//   // Methods run on jQuery selectors return the selector they we run on
+//   var firstNameTd = $("<td id='firstName-display'>").text(child.First_name);
+//   var lastNameTd = $("<td id='lastName-display'>").text(child.Last_name);
+//   var startDateTd = $("<td id='startDate-display'>").text(child.Email);
+//   var salaryTd = $("<td id='salary-display'>").text(child.Trip_date);
+//   var deginationTd = $("<td id='degination-display'>").text(child.Phone_number);
+//   var emailTd = $("<td id='email-display'>").text(child.Date_purchased);
+//   var deleteTd = $("<td id='delete-display'>").text(child.Location);
+
+//   // Append the newly created table data to the table row
+//   tRow.append(
+//     firstNameTd,
+//     lastNameTd,
+//     startDateTd,
+//     salaryTd,
+//     deginationTd,
+//     emailTd,
+//     deleteTd
+//   );
+//   // Append the table row to the table body
+//   $("tbody").append(tRow);
 // }

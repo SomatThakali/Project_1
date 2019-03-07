bd1431b1170c51036c5d4a91522becf74a791c72
JKD22 <46582090+JKD22@users.noreply.github.com>
2019-03-07 13:45:20 -0500

    broken map btns

--- assets/javascript/googlemaps.js
+++ assets/javascript/googlemaps.js
@@ -3,22 +3,32 @@ var latVal = [40.774309349999996, 40.75361345, 40.74770384999999];
 var lngVal = [-73.9708367973161, -73.97658004429898, -74.00489117085408];
 var locId = ["loc1", "loc2", "loc3"];
 var locTitle = ["Central Park", "Grand Central Station", "The High Line"];
-console.log("map initialized");
 /** Initializes the map and the custom popup. */
 function initMap() {
+  console.log("map initialized");
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.762009349999996, lng: -73.9858367973161},
     zoom: 13,
   });
-for (i = 0; i < locTitle.length; i++){
-  Popup = createPopupClass();
-  popup = new Popup(
+  for (i = 0; i < locTitle.length; i++){
+    console.log("popup " + i + " created");
+    Popup = createPopupClass();
+    popup = new Popup(
       new google.maps.LatLng(latVal[i], lngVal[i]),
       document.getElementById(locId[i]));
-  popup.setMap(map);
-}
-}
-
+      popup.setMap(map);
+    }
+  }
+  // Jquery btn functionality
+  var bookBtn = "<a href='signUp.html' class='btn btn-warning book-now'>Book Now</a>";
+  var learnBtn = "<a href='learn_gc.html' class='btn btn-dark learn-more'>Learn More</a>";
+   
+  $("#loc1Btn").on('click', function() {
+      console.log("part 1 init");
+      $("#loc1").html(
+        bookBtn + "<span>" + learnBtn
+      );
+    });
 /**
  * Returns the Popup class.
  *
@@ -88,3 +98,4 @@ function createPopupClass() {
 
   return Popup;
 }
+
